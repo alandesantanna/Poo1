5e426912ec2f7baaa77e9ab8b7109828bf582fbb
Alan de Sant'Anna <108240669+alandesantanna@users.noreply.github.com>
2024-09-16 21:44:36 -0300

    estudos de poo
    16/09

---
+++ Programacao-orientada-objetos1/src/pooAula3/Aula3.java
@@ -0,0 +1,11 @@
+package pooAula3;
+
+public class Aula3 {
+    public static void main(String[] args) {
+        Caneta c1 = new Caneta("NIC", "Amarelo", 0.4f);
+        c1.status();
+
+        Caneta c2 = new Caneta("KKK", "Laranja", 1.5f);
+        c2.status();
+    }
+}
