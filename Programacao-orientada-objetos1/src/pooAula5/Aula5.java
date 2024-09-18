c7f21211a82c60cbcd79110da6a935e3f0546c27
Alan de Sant'Anna <108240669+alandesantanna@users.noreply.github.com>
2024-09-18 20:05:41 -0300

    aula 5 18/09
    18/09 20:05

---
+++ Programacao-orientada-objetos1/src/pooAula5/Aula5.java
@@ -0,0 +1,12 @@
+package pooAula5;
+
+public class Aula5 {
+    public static void main(String[] args) {
+        ControleRemoto c = new ControleRemoto();
+        c.maisVolume();
+        c.play();
+        c.abrirMenu();
+        c.fecharMenu();
+
+    }
+}
