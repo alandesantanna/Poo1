24346eaa238ab2dd63ad5e41aa5c90094a7784af
Alan de Sant'Anna <108240669+alandesantanna@users.noreply.github.com>
2024-09-26 01:29:37 -0300

    26/09
    26/09 - 01:30

---
+++ Programacao-orientada-objetos1/src/pooAula7/Aula7.java
@@ -0,0 +1,23 @@
+package pooAula7;
+
+public class Aula7 {
+    public static void main(String[] args) {
+        Pessoa[] p = new Pessoa[2];
+        Livro[] l = new Livro[3];
+
+        p[0] = new Pessoa("Pedro", 22, "M");
+        p[1] = new Pessoa("Maria", 25, "F");
+
+        l[0] = new Livro("Aprendendo a ser rico","tchola", 300, p[0]);
+
+        l[1] = new Livro("Poo para iniciantes", "Pedro paulo", 500, p[1]);
+
+
+        l[2] = new Livro("Java avançado","Maria Candido", 800, p[0]);
+
+        l[0].abrir();
+        l[0].folhear(400);
+
+        System.out.println(l[0].detalhes()); 
+    }    
+}
